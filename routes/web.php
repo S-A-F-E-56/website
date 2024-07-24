a0c1d9e839aa7c8d56cc6cd2915a4e57b3295e8d
richardwijaya04 <richardwijayaharianto@gmail.com>
2024-07-24 19:11:37 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
+use App\Http\Controllers\experienceController;
+use App\Http\Controllers\halamanController;
+use Laravel\Socialite\Facades\Socialite;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('home', 'dashboard');
+
+Route::get('/auth', [authController::class, "index"])->name('login')->middleware('guest');
+Route::get('/auth/redirect', [authController::class, "redirect"])->middleware('guest');
+Route::get('/auth/callback', [authController::class, "callback"])->middleware('guest');
+Route::get('/auth/logout', [authController::class, "logout"]);
+
+Route::prefix('dashboard')->middleware('auth')->group(
+    function(){
+        Route::get('/', [halamanController::class, 'index']);
+        Route::resource('halaman', halamanController::class);
+        Route::resource('experience', experienceController::class);
+    }
+);
